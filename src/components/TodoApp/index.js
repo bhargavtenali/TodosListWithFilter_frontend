6c5d609dd40a5e97492cd9edd82d0b822280e06e
bhargavtenali <bhargavtenali@gmail.com>
2022-08-09 22:27:31 +0530

    c

--- src/components/TodoApp/index.js
+++ src/components/TodoApp/index.js
@@ -3,22 +3,59 @@ import "./index.css";
 import TodoItem from "../TodoItem";
 import { v4 } from "uuid";
 
-function TodoApp() {
-    const [todosList, changeTodos] = useState([]);
-    const [input, changeInput] = useState("");
+const STATUS_CONSTANTS = { COMPLETE: "complete", INCOMPLETE: "incomplete" }
 
-    const addTodoHandler = () => {
-        const newTodoItem = { id: v4(), input, isComplete: false };
-        changeTodos((prevTodosList) => [...prevTodosList, newTodoItem]);
-        changeInput("");
+function TodoApp() {
+    const [todosList, changeTodosList] = useState(JSON.parse(localStorage.getItem("todolist")));
+    const [name, changeName] = useState("");
+    const [description, changeDescription] = useState("");
+    const [time, changeTime] = useState("");
+    const [status, changeStatus] = useState(STATUS_CONSTANTS.INCOMPLETE)
+    const [displayFilter, changedisplayFilter] = useState("all")
+
+    const addTodoHandler = (e) => {
+        e.preventDefault()
+        const newTodoItem = {
+            id: v4(), name, description, time,
+            isComplete: (status === STATUS_CONSTANTS.COMPLETE ? true : false)
+        };
+        const actualTodosList = JSON.parse(localStorage.getItem("todolist"))
+        const newActualTodosList = [...actualTodosList, newTodoItem]
+        localStorage.setItem("todolist", JSON.stringify(newActualTodosList))
+        const filteredTodos = newActualTodosList.filter((item) => {
+            if (displayFilter === "all") {
+                return true
+            }
+            else {
+                return ((item.isComplete ? STATUS_CONSTANTS.COMPLETE : STATUS_CONSTANTS.INCOMPLETE) === displayFilter)
+            }
+        })
+        changeTodosList(filteredTodos)
+        changeName("");
+        changeDescription("");
+        changeTime("");
+        changeStatus(STATUS_CONSTANTS.INCOMPLETE);
     };
+
     const deleteTodoHandler = (currentID) => {
-        const newTodos = todosList.filter((item) => item.id !== currentID);
-        changeTodos(newTodos);
+        const actualTodosList = JSON.parse(localStorage.getItem("todolist"))
+        const newActualTodosList = actualTodosList.filter((item) => item.id !== currentID);
+        localStorage.setItem("todolist", JSON.stringify(newActualTodosList))
+        const filteredTodos = newActualTodosList.filter((item) => {
+            if (displayFilter === "all") {
+                return true
+            }
+            else {
+                return ((item.isComplete ? STATUS_CONSTANTS.COMPLETE : STATUS_CONSTANTS.INCOMPLETE) === displayFilter)
+            }
+        })
+        changeTodosList(filteredTodos)
+
     };
 
     const changeTodoCompleteHandler = (currentID) => {
-        const newTodos = todosList.map((item) => {
+        const actualTodosList = JSON.parse(localStorage.getItem("todolist"))
+        const newActualTodosList = actualTodosList.map((item) => {
             if (item.id === currentID) {
                 return { ...item, isComplete: !item.isComplete };
             } else {
@@ -26,30 +63,125 @@ function TodoApp() {
             }
         });
         // console.log(newTodos[0].isComplete);
-        changeTodos(newTodos);
+        localStorage.setItem("todolist", JSON.stringify(newActualTodosList))
+        const filteredTodos = newActualTodosList.filter((item) => {
+            if (displayFilter === "all") {
+                return true
+            }
+            else {
+                return ((item.isComplete ? STATUS_CONSTANTS.COMPLETE : STATUS_CONSTANTS.INCOMPLETE) === displayFilter)
+            }
+        })
+        changeTodosList(filteredTodos)
     };
+
+    const changeFilterHandler = (event) => {
+        const currentFilterValue = event.target.value
+        const actualTodosList = JSON.parse(localStorage.getItem("todolist"))
+        const filteredTodos = actualTodosList.filter((item) => {
+            if (currentFilterValue === "all") {
+                return true
+            }
+            else {
+                return ((item.isComplete ? STATUS_CONSTANTS.COMPLETE : STATUS_CONSTANTS.INCOMPLETE) === currentFilterValue)
+            }
+        })
+        changeTodosList(filteredTodos)
+        changedisplayFilter(currentFilterValue)
+    }
+
 
     return (
         <div className="bg-container">
-            <h4>Todos List</h4>
-            <input
-                value={input}
-                onChange={(e) => changeInput(e.target.value)}
-                type="text"
-            />
-            {"   "}
-            <button onClick={addTodoHandler}>Add Todo</button>
-            <br />
-            <br />
-            {todosList.map((item) => (
-                <TodoItem
-                    key={item.id}
-                    item={item}
-                    deleteTodoHandler={deleteTodoHandler}
-                    changeTodoCompleteHandler={changeTodoCompleteHandler}
-                />
-            ))}
-        </div>
+            <div className="app-container" >
+                <form className="form-container" onSubmit={addTodoHandler}>
+                    <h1 className="heading" >Add TODO</h1>
+                    <div className="input-container" >
+                        <label className="labelStyle" htmlFor="name" >Title</label>
+                        {"  "}
+                        <input
+                            className="inputStyle"
+                            id="name"
+                            value={name}
+                            onChange={(e) => changeName(e.target.value)}
+                            type="text"
+                        />
+                    </div>
+                    <div className="input-container" >
+                        <label className="labelStyle" htmlFor="description" >Description</label>
+                        {"  "}
+                        <input
+                            className="inputStyle"
+                            id="description"
+                            value={description}
+                            onChange={(e) => changeDescription(e.target.value)}
+                            type="text"
+                        />
+                    </div>
+                    <div className="input-container">
+                        <label className="labelStyle" htmlFor="time">Time</label>
+                        {"  "}
+                        <input
+                            className="inputStyle"
+                            id="time"
+                            value={time}
+                            onChange={(e) => changeTime(e.target.value)}
+                            type="time"
+                        />
+                    </div>
+                    <div className="input-container">
+                        <div>
+                            <input
+                                id="complete"
+                                name="status"
+                                value={STATUS_CONSTANTS.COMPLETE}
+                                onChange={(e) => changeStatus(e.target.value)}
+                                type="radio"
+                                checked={status === STATUS_CONSTANTS.COMPLETE}
+                            />
+                            <label className="labelStyle" htmlFor="complete" >Complete</label>
+                        </div>
+                        <div>
+                            <input
+                                id="incomplete"
+                                name="status"
+                                value={STATUS_CONSTANTS.INCOMPLETE}
+                                onChange={(e) => changeStatus(e.target.value)}
+                                type="radio"
+                                checked={status === STATUS_CONSTANTS.INCOMPLETE}
+                            />
+                            <label className="labelStyle" htmlFor="incomplete" >Not Complete</label>
+                        </div>
+                    </div>
+                    <button className="button" type="submit">Add Todo</button>
+                </form>
+                <div className="todoList-container">
+                    <h1 className="heading">TODO LIST</h1>
+                    <div className="input-container" >
+                        <label className="labelStyle" htmlFor="todoListView" >Filter</label>
+                        <select
+                            className="inputStyle"
+                            value={displayFilter}
+                            onChange={changeFilterHandler}
+                            name="todoListView" id="todoListView">
+                            <option value="all" >All</option>
+                            <option value={STATUS_CONSTANTS.COMPLETE}>Already Completed</option>
+                            <option value={STATUS_CONSTANTS.INCOMPLETE}>InComplete</option>
+                        </select>
+                    </div>
+                    <div className="todo-container">
+                        {todosList.map((item) => (
+                            <TodoItem
+                                key={item.id}
+                                item={item}
+                                deleteTodoHandler={deleteTodoHandler}
+                                changeTodoCompleteHandler={changeTodoCompleteHandler}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div >
     );
 }
 
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,22 +1,26 @@
 import "./index.css";
 
 function TaskItem({ item, deleteTodoHandler, changeTodoCompleteHandler }) {
+    const { name, description, time, isComplete } = item
     return (
-        <div>
+        <div className="todoItemContainer">
             <input
+                className="checkBox"
                 id={item.id}
                 type="checkbox"
-                checked={item.isComplete}
+                checked={isComplete}
                 onChange={() => changeTodoCompleteHandler(item.id)}
             />
             <label
                 htmlFor={item.id}
-                className={item.isComplete ? "completeClass" : "incompleteClass"}
+                className={isComplete ? "labelStyleChild completeClass" : "labelStyleChild incompleteClass"}
             >
-                {item.input}
+                <p className="text1" >{name}</p>
+                <p className="text" >{description}</p>
+                <p className="text2" >{time}</p>
             </label>
             {"   "}
-            <button onClick={() => deleteTodoHandler(item.id)}>Delete</button>
+            <button className="button2" onClick={() => deleteTodoHandler(item.id)}>Delete</button>
         </div>
     );
 }
